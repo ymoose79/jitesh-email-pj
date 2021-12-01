8ed81edb4aa358e63bb4c123166d4aeca107ceac
ymoose79 <justin.r.stock@icloud.com>
2021-12-01 16:39:28 -0500

    G.Button but with 401 error

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,14 @@
 <script>
-
+	import GoogleSignInButton from './UI/GoogleSignInButton.svelte'
 	export let name;
+
+	
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<GoogleSignInButton/>
 </main>
 
 <style>
---
+++ src/UI/GoogleSignInButton.svelte
@@ -0,0 +1,14 @@
+<script>
+    window.onSignIn = (googleUser) => {
+       const profile = googleUser.getBasicProfile();
+       console.log('ID: ' + profile.getId());
+       console.log('Name: ' + profile.getName());
+       console.log('Image URL: ' + profile.getImageUrl());
+       console.log('Email: ' + profile.getEmail());
+    };
+  </script>
+<svelte:head>
+  <script src="https://apis.google.com/js/platform.js" async defer></script>
+</svelte:head>
+
+<div class="g-signin2" data-longtitle="true" data-onsuccess="onSignIn" />
