9b12c73b0242dbdbf11225ab439784cc4039bf0a
ymoose79 <justin.r.stock@icloud.com>
2021-12-02 10:08:37 -0500

    working login

--- src/UI/GoogleSignInButton.svelte
+++ src/UI/GoogleSignInButton.svelte
@@ -6,6 +6,9 @@
        console.log('Image URL: ' + profile.getImageUrl());
        console.log('Email: ' + profile.getEmail());
     };
+    const metaTag = document.querySelector('meta[name="google-signin-client_id"]')
+    metaTag.content = "159286373359-geta9ia939ah6np21elk8ol2j7rgv4tr.apps.googleusercontent.com"
+
   </script>
 <svelte:head>
   <script src="https://apis.google.com/js/platform.js" async defer></script>
