75bcf1f634c4053d68e4fff199f5565733aa551a
ymoose79 <justin.r.stock@icloud.com>
2021-12-04 17:48:44 -0500

    workin progress..

--- src/UI/GoogleSignInButton.svelte
+++ src/UI/GoogleSignInButton.svelte
@@ -5,12 +5,12 @@
     'meta[name="google-signin-client_id"]'
   );
 
-  let btnSignInOut;
-  let btnRevokeAccess;
+  let btnSignIn;
+  let btnSignOut;
   let authStatus;
-
-  
-
+  let isAuthorized;
+  let gBtn;
+  let signOutBtn;
   metaTag.content = CLIENT_ID;
 
   // window.onSignIn = (googleUser) => {
@@ -21,136 +21,102 @@
   //   console.log("Email: " + profile.getEmail());
   // };
 
-
   var GoogleAuth;
-      var SCOPE = "https://www.googleapis.com/auth/drive.metadata.readonly";
-      function handleClientLoad() {
-        // Load the API's client and auth2 modules.
-        // Call the initClient function after the modules load.
-        console.log('handle client')
-        gapi.load("client:auth2", initClient);
-        
-      }
-      
-      function initClient() {
-        // In practice, your app can retrieve one or more discovery documents.
-        var discoveryUrl =
-        "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest";
-        
-        // Initialize the gapi.client object, which app uses to make API requests.
-        // Get API key and client ID from API Console.
-        // 'scope' field specifies space-delimited list of access scopes.
-        gapi.client
-          .init({
-            apiKey: "AIzaSyDlWLse9HDF_8vwgv9B-zt1xPpxK0YLYp8",
-            clientId: CLIENT_ID,
-            discoveryDocs: [discoveryUrl],
-            scope: SCOPE,
-          })
-          .then(function () {
-            GoogleAuth = gapi.auth2.getAuthInstance();
-            // Listen for sign-in state changes.
-            GoogleAuth.isSignedIn.listen(updateSigninStatus);
+  var SCOPE = "https://www.googleapis.com/auth/drive.metadata.readonly";
 
-            // Handle initial sign-in state. (Determine if user is already signed in.)
-            var user = GoogleAuth.currentUser.get();
-            setSigninStatus();
-            console.log(user)
-            // Call handleAuthClick function when user clicks on
-            //      "Sign In/Authorize" button.
-            btnSignInOut.click(function () {
-              handleAuthClick();
-            });
-            btnRevokeAccess.click(function () {
-              revokeAccess();
-            });
-          });
-      }
+  function handleClientLoad() {
+    console.log("client load.... hahahaha");
+    gapi.load("client:auth2", initClient);
+  }
 
-      function handleAuthClick() {
-        if (GoogleAuth.isSignedIn.get()) {
-          // User is authorized and has clicked "Sign out" button.
-          GoogleAuth.signOut();
-        } else {
-          // User is not signed in. Start Google auth flow.
-          GoogleAuth.signIn();
-        }
-      }
+  function initClient() {
+    // generates methods the application can use
+    var discoveryUrl =
+      "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest";
 
-      function revokeAccess() {
-        GoogleAuth.disconnect();
-      }
+    gapi.client
+      .init({
+        apiKey: "AIzaSyDlWLse9HDF_8vwgv9B-zt1xPpxK0YLYp8",
+        clientId: CLIENT_ID,
+        discoveryDocs: [discoveryUrl],
+        scope: SCOPE,
+      })
+      .then(function () {
+        GoogleAuth = gapi.auth2.getAuthInstance();
+        GoogleAuth.signIn();
+        GoogleAuth.isSignedIn.listen(updateSigninStatus);
+      });
+  }
+  /**
+   * Store the request details. Then check to determine whether the user
+   * has authorized the application.
+   *   - If the user has granted access, make the API request.
+   *   - If the user has not granted access, initiate the sign-in flow.
+   */
+  function sendAuthorizedApiRequest(requestDetails) {
+    currentApiRequest = requestDetails;
+    if (isAuthorized) {
+      // Make API request
+      // gapi.client.request(requestDetails)
 
-      function setSigninStatus() {
-        var user = GoogleAuth.currentUser.get();
-        console.log("user:******", user)
-        var isAuthorized = user.hasGrantedScopes(SCOPE);
-        if (isAuthorized) {
-          btnSignInOut.innerHTML = "Sign out";
-          btnRevokeAccess.classList.remove('hide')
-          authStatus.innerHTML = 
-            "You are currently signed in and have granted " +
-              "access to this app."
-          ;
-        } else {
-          btnSignInOut.innerHTML = "Sign In/Authorize";
-          btnRevokeAccess.classList.add('hide');
-          authStatus.innerHTML = 
-            "You have not authorized this app or you are " + "signed out."
-          ;
-        }
-      }
+      // Reset currentApiRequest variable.
+      currentApiRequest = {};
+    } else {
+      GoogleAuth.signIn();
+    }
+  }
 
-      function updateSigninStatus() {
-        setSigninStatus();
+  /**
+   * Listener called when user completes auth flow. If the currentApiRequest
+   * variable is set, then the user was prompted to authorize the application
+   * before the request executed. In that case, proceed with that API request.
+   */
+  function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+      isAuthorized = true;
+      if (currentApiRequest) {
+        sendAuthorizedApiRequest(currentApiRequest);
       }
+    } else {
+      isAuthorized = false;
+    }
+  }
+  function notAuthroized(){
+    isAuthorized = false;
+    gBtn.classList.remove('--hide')
+    signOutBtn.classList.add('--hide')
+    updateSigninStatus();
+  }
 </script>
 
-<!-- src switched -->
 <!-- src="https://apis.google.com/js/api.js" -->
 <svelte:head>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script
-  async
-  defer
-  src="https://apis.google.com/js/platform.js?onload=init"
-  on:load={function(){}}
-  onreadystatechange="if (this.readyState === 'complete') this.onload()"
-></script>
-
-
-  <!-- <script
-    src="https://apis.google.com/js/platform.js?onload=init"
+    src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script
     async
-    defer></script> -->
-  <!-- <script src="https://apis.google.com/js/platform.js" async defer></script> -->
+    defer
+    src="https://apis.google.com/js/platform.js?onload=init"
+    on:load={function () {
+      console.log("nameless function");
+    }}>
+  </script>
 
-
+  <script src="https://apis.google.com/js/platform.js" async defer></script>
 </svelte:head>
+<svelte:window on:load={handleClientLoad} />
 
-<button id="signin" bind:this={btnSignInOut} on:click={handleClientLoad}>
-  Sign In/Authorize
-</button>
-<button id="signout" class="hide" bind:this={btnRevokeAccess} on:click={handleClientLoad}>
-  Revoke access
-</button>
-
-<div id="auth-status" bind:this={authStatus} ></div>
-<hr />
-
+<h2 class="alert alert-primary">sign in with google</h2>
+<div class="g-signin2 --hide" on:bind={gBtn}/>
+<button id="signout" on:click={notAuthroized} on:bind={signOutBtn}>sign out</button>
 
 <style>
-  #signout,
-  #signin {
-    margin-left: 25px;
-  }
-  .hide {
+  .--hide {
     display: none;
   }
-  #auth-status{
-    display: inline;
-    padding-left: 25px;
+  .g-signin2 {
+    position: absolute;
+    top: 50%;
+    left: 50%;
   }
 </style>
-
-
