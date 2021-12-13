cc058a344cdf1d26c4531009cb469536c604ade7
ymoose79 <justin.r.stock@icloud.com>
2021-12-13 17:25:58 -0500

    added body, waiting for multiple attachments

--- src/UI/SendEmailButton.svelte
+++ src/UI/SendEmailButton.svelte
@@ -1,14 +1,56 @@
 <script>
   export let to = "justin.r.stock@gmail.com";
   export let name = "Justin Stock";
+  export let body = ""
 
   let files = "";
 
-  function loadGmailApi() {
-    gapi.client.load("gmail", "v1", formatFile());
+  const toBase64 = (file) =>
+    new Promise((res, rej) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => res(reader.result);
+      reader.onerror = (err) => rej(err);
+    });
+
+
+  async function sendEmail() {
+    if (!files[1]) {
+      const file = files[0];
+      const data = await toBase64(file);
+      const dataArr = data.split(',')
+     const x = dataArr.shift()
+     const readyToSendData = dataArr.join();
+      formatFile(readyToSendData);
+    }
   }
 
-  function sendsEmail(multipleFiles) {
+  function formatFile(data) {
+    // ---------------- send 1 attachment
+    let multipleFiles = "";
+    if (!files[1]) {
+      multipleFiles += `Content-Type: ${files[0].type} name="${files[0].name}"\n`;
+      multipleFiles += `Content-Transfer-Encoding: base64\n`;
+      multipleFiles += `Content-Disposition: attachment; filename=${files[0].name}\n\n`;
+
+      multipleFiles += data;
+
+      forwardAttachments(multipleFiles);
+    }
+    // ---------------- send multiple attachment
+    // else {
+    //   for (let i = 0, numFiles = files.length; i < numFiles; i++) {
+    //     multipleFiles += "--foo_bar_baz\n";
+    //     multipleFiles += `Content-type: ${files[i].type} name="${files[i].name}"\n`;
+    //     multipleFiles += `Content-Disposition: attachment; filename=${files[i].name}\n\n`;
+    //     multipleFiles += `waiting on data....\n\n`;
+    //   }
+    //   forwardAttachments(multipleFiles);
+    // }
+  }
+
+
+  function forwardAttachments(multipleFiles) {
     let base64EncodedEmail = btoa(
       //   `Content-Type:  text/plain; charset="UTF-8"
       `Content-Type: multipart/mixed; boundary="foo_bar_baz"
@@ -20,7 +62,8 @@ Subject: Hello world
 --foo_bar_baz
 Content-Type: text/plain
 
-The actual message text goes here
+${body}
+
 --foo_bar_baz
 ${multipleFiles}
 --foo_bar_baz--
@@ -35,35 +78,11 @@ ${multipleFiles}
         raw: base64EncodedEmail,
       },
     });
-    request.execute(function (res) {
-      console.log(res);
-    });
-  }
-
-  function formatFile() {
-    // ---------------- send 1 attachment
-    let multipleFiles = "";
-    if (!files[1]) {
-      multipleFiles += `Content-type: ${files[0].type} name="${files[0].name}"\n`;
-      multipleFiles += `Content-Disposition: attachment; filename=${files[0].name}\n\n`;
-      multipleFiles += `waiting on data....`;
-
-      sendsEmail(multipleFiles);
-    }
-    // ---------------- send multiple attachment
-    else {
-      for (let i = 0, numFiles = files.length; i < numFiles; i++) {
-        multipleFiles += "--foo_bar_baz\n";
-        multipleFiles += `Content-type: ${files[i].type} name="${files[i].name}"\n`;
-        multipleFiles += `Content-Disposition: attachment; filename=${files[i].name}\n\n`;
-        multipleFiles += `waiting on data....`;
-      }
-      sendsEmail(multipleFiles);
-    }
+    request.execute();
   }
 </script>
 
-<form on:submit|preventDefault={loadGmailApi}>
+<form on:submit|preventDefault={sendEmail}>
   <input type="file" bind:files multiple />
   <label
     >To:
@@ -73,5 +92,9 @@ ${multipleFiles}
     >From:
     <input type="text" placeholder="FROM name" bind:value={name} />
   </label>
+  <label
+  >Body:
+  <textarea rows="10" cols="50" bind:value={body} />
+</label>
   <button>Send Email</button>
 </form>
